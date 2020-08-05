43bec661e76cf27515a47dae3ef4eaa043c21263
Ricardo <manosalvas2013@gmail.com>
2020-08-05 06:40:18 -0400

    db created

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const conTab = require("console.table");
 
-const connection = mysql.createConnection({
-  host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+var connection = mysql.createConnection({
+    host: "localhost",
 
-  // Your username
-  user: "root",
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your password
-  password: "password123",
-  database: "employeeDB"
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password123",
+    database: "employeeDB"
 });
+
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    } else {
+        init();
+    }
+});
+
+function init() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "firstQ",
+            message: "What would you like to do?",
+            choices: [
+                
+            ]
+        })
+}
